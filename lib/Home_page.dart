5dff580619cb15c63d62745195d7bd3d92ee0f86
siddharthsharma001 <siddharth.sharma.coder@gmail.com>
2023-07-31 02:52:13 +0530

    day 2 completed

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -1,21 +1,30 @@
-
 import 'package:flutter/material.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
-  
+
   final int days = 1000;
 
   final String name = "web Development";
 
-
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(
+    const String apkname = "CataLog App";
+
+
+    return Scaffold(
+      // app title and menu
+      appBar: AppBar(
+        title: const Text(apkname),
+      ),
+
+      // main / body of all 
+      body: Center(
         child: Text("$days days of code of $name"),
       ),
+
+      // used to make the drawer menu
+      drawer: Drawer(),
     );
-    
   }
 }
