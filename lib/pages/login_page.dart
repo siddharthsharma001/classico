44d09b9f656d9e12c719f9a61b553409eaf9705b
siddharthsharma001 <siddharth.sharma.coder@gmail.com>
2023-07-31 22:06:38 +0530

    day6 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,16 @@
 import 'package:classico/utils/routes.dart';
 import 'package:flutter/material.dart';
 
-class loginPage extends StatelessWidget {
+class loginPage extends StatefulWidget {
   const loginPage({super.key});
 
+  @override
+  State<loginPage> createState() => _loginPageState();
+}
+
+class _loginPageState extends State<loginPage> {
+  String name = "";
+  bool isbuttonclicked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,9 +28,10 @@ class loginPage extends StatelessWidget {
             const SizedBox(
               height: 20.0,
             ),
-            const Text(
-              "Welcome",
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            Text(
+              "Welcome $name",
+              style:
+                  const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
             ),
             Padding(
               padding:
@@ -35,6 +43,10 @@ class loginPage extends StatelessWidget {
                       hintText: "Enter Username",
                       labelText: "Username",
                     ),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                   ),
                   TextFormField(
                     obscureText: true,
@@ -46,11 +58,37 @@ class loginPage extends StatelessWidget {
                   const SizedBox(
                     height: 20.0,
                   ),
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, myRoutes.homeRoute);
-                      },
-                      child: const Text("Login")),
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        isbuttonclicked = true;
+                      });
+                      await Future.delayed(const Duration(seconds: 1));
+                      Navigator.pushNamed(context, myRoutes.homeRoute);
+                    },
+                    child: AnimatedContainer(
+                      duration: const Duration(seconds: 1),
+                      width: isbuttonclicked ? 50 : 100,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius:
+                            BorderRadius.circular(isbuttonclicked ? 50 : 0),
+                      ),
+                      child: Center(
+                          child: isbuttonclicked
+                              ? const Icon(
+                                  Icons.done,
+                                  color: Colors.white,
+                                )
+                              : const Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                )),
+                    ),
+                  ),
                 ],
               ),
             )
@@ -58,7 +96,7 @@ class loginPage extends StatelessWidget {
         ),
       ),
       drawer: const Drawer(
-        child: Center(child: Text("Teri maa ki chut rajinder")),
+        child: Center(child: Text("This App is made by siddharth Sharma")),
       ),
     );
   }
