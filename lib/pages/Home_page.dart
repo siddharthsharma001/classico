9a8182cb60682ba57653b769d9617a56de3b8989
siddharthsharma001 <siddharth.sharma.coder@gmail.com>
2023-08-01 14:06:03 +0530

    day 8 commit

--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -1,5 +1,8 @@
+import 'package:classico/Models/catalog.dart';
 import 'package:classico/widgets/drawer.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/items_widgets.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
@@ -10,12 +13,23 @@ class Homepage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text(apkname , textScaleFactor: 1.3, style: TextStyle(fontWeight: FontWeight.bold),),
+        title: const Text(
+          apkname,
+          textScaleFactor: 1.3,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
       ),
-      body: const Center(
-        child: Text("Welcome to 30 days of code"),
+      body: ListView.builder(
+        itemCount: CatalogModel.items.length,
+        itemBuilder: (context, index) {
+          // item builder will give us a index or position
+          // catalog model ke andar ek hi element hai aur uska index 0 hai
+          return itemWidget(item: CatalogModel.items[index],);
+        },
       ),
       drawer: const MyDrawer(),
     );
   }
+  
+  
 }
---
+++ lib/widgets/items_widgets.dart
@@ -0,0 +1,35 @@
+import 'package:classico/Models/catalog.dart';
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class itemWidget extends StatelessWidget {
+  const itemWidget({super.key, required this.item});
+  final Item item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
+      child: Card(
+        elevation: 10.0,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 20.0),
+          child: ListTile(
+            onTap: () {
+              print("item ${item.name} pressed");
+            },
+            leading: Image.network(item.image),
+            title: Text(item.name),
+            subtitle: Text(item.desc),
+            trailing: Text(
+              "₹ ${item.price}/-",
+              textScaleFactor: 1.1,
+              style: const TextStyle(
+                  color: Colors.deepPurple, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
